137bdb37a49a2d15377402f960f3fc72fb95fdf1
sbocce <dainonottambulo@gmail.com>
2016-09-16 18:39:19 +0100

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <cstdlib> // for rand() and srand()
 
 class particle {
   public:
@@ -12,6 +12,7 @@ class particle {
 class cell {
   public:
     double XYZcorners[6];
+    std::vector< int > neighbors;            // <<<<<---------------  !! Important!
     std::vector< particle > particles;
 
     void compute_sides();
@@ -36,7 +37,8 @@ int main()
   double x_max = 8.;
 
  
-  cell* p_cell_now; // Pointer to cells
+  cell* p_cell_now;  // Pointer to cells
+  particle part_now; // A particle to be copied..
 
   for(size_t ii = 0; ii < N_cells; ++ii) {
     p_cell_now = &cells_vec.at(ii);   // Set pointer to current cell
@@ -51,13 +53,32 @@ int main()
     // Setting Z of corners
     p_cell_now->XYZcorners[4] = -0.5;
     p_cell_now->XYZcorners[5] =  0.5;
+
+
+    // ------- Fill it with a random number of particles -------
+
+    // for ii = 1 : n_particles
+      // Create a particle at random position in the cell
+      double rand_now1 = rand() / double(RAND_MAX);
+      double rand_now2 = rand() / double(RAND_MAX);
+      double rand_now3 = rand() / double(RAND_MAX);
+      part_now.pos[0] = p_cell_now->XYZcorners[0] + \
+                        rand_now1*(p_cell_now->XYZcorners[1] - p_cell_now->XYZcorners[0]);  // x
+      part_now.pos[1] = p_cell_now->XYZcorners[2] + \
+                        rand_now2*(p_cell_now->XYZcorners[3] - p_cell_now->XYZcorners[2]);  // y
+      part_now.pos[2] = p_cell_now->XYZcorners[4] + \
+                        rand_now3*(p_cell_now->XYZcorners[5] - p_cell_now->XYZcorners[4]);  // z
+
+      // Give to the particle a random velocity
+//      double rand_now4 = rand() / double(RAND_MAX);
+//      double rand_now5 = rand() / double(RAND_MAX);
+//      double rand_now6 = rand() / double(RAND_MAX);
+//      part_now.vel[0] = 
+     
+      // Push the particle into the cell 
+      p_cell_now->particles.push_back(part_now);
   }                      
 
-  for(size_t ii = 0; ii < N_cells; ++ii) {
-    std::cout << "X: " << cells_vec[ii].XYZcorners[0] << "   \n";
-  }
-                         
 
-  std::cout << "Ciao! Celle: " << cells_vec.size() << "\n";
   return 0;
 }
