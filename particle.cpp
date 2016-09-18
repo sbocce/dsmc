5c39fb424e8b4ebf0831fe218fded23408eb9899
sbocce <dainonottambulo@gmail.com>
2016-09-18 22:32:28 +0100

    new file:   particle.cpp 	modified:   Makefile 	modified:   main.cpp 	modified:   particle.h 	modified:   solver.cpp 	modified:   solver.h 	modified:   translation_step_2D.jpg

---
+++ particle.cpp
@@ -0,0 +1,8 @@
+#include "particle.h"
+
+void particle::advect(double dt, double new_pos[])
+{
+  new_pos[0] = pos[0] + vel[0]*dt;
+  new_pos[1] = pos[1] + vel[1]*dt;
+  new_pos[2] = pos[2] + vel[2]*dt;
+}
