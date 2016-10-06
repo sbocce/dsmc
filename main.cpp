bee53bb1f71c4951cb034b422ba6ecf8e348f6c0
Stefano Bocce <wk6@hotmail.com>
2016-10-06 11:29:43 +0200

    Added exporter object

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include "mesh.h"
+#include "exporter.h"
 #include "solver.h"
 
 
@@ -12,16 +13,17 @@ int main()
   Msh.set_domain_box(-1.5, 5, 0, 1, -0.5, 0.5);   // Set outer compuational domain
   Msh.create();
 
-  // Export mesh
-  Msh.export_VTK();
+  // 2) Create exporter object
+  exporter Exptr(&Msh);
+  Exptr.export_mesh_VTK("VTK_mesh.vtk"); 
 
-  // 2) Create solver object
+  // 3) Create solver object
   solver Slvr(&Msh);
   Slvr.initialize();
   Slvr.seed_particles();
 
   Slvr.translation_step();
-
+  Exptr.export_partpercell_VTK(); // for debugging reasons
 
 
   return 0;
