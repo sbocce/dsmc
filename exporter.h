d57f848646ee298a12076af8e050020611e36121
Stefano Bocce <wk6@hotmail.com>
2016-10-06 11:26:10 +0200

    modified:   Makefile 	modified:   cell.cpp 	modified:   cell.h 	modified:   main.cpp 	modified:   mesh.cpp 	modified:   mesh.h 	modified:   solver.cpp 	modified:   solver.h

---
+++ exporter.h
@@ -0,0 +1,18 @@
+#ifndef EXPORTER_H
+#define EXPORTER_H
+
+#include "mesh.h"
+
+class exporter {
+
+  public:
+    exporter(mesh* p_msh);
+    ~exporter(){};
+    void export_mesh_VTK(const char* filename);
+    void export_partpercell_VTK(); // TEMPORARY EXPORTING FUNCTION
+
+  private:
+    mesh*   p_mesh;
+};
+
+#endif /* EXPORTER_H */
