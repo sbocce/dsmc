af749d8190bf1e652fb1fc2b278799b3b234c8c9
Stefano Bocce <wk6@hotmail.com>
2016-10-26 21:13:44 +0200

    new file:   src/constants.h

---
+++ src/constants.h
@@ -0,0 +1,7 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+const double kB = 1.38064852e-24;                 // Boltzmann's constant [J/K]
+const double pi = 3.141592653589793238462643383;
+
+#endif /* CONSTANTS_H */
